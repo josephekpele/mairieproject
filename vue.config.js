9772df76bbab39ff559ac4c84faef48ec9afefa1
devsoy <soymobiledev@gmail.com>
2021-04-26 17:19:17 +0200

    correction du vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,4 @@
-
 module.exports = {
-  pwa: {
-    name: 'My App',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'src/registerServiceWorker.js',
-      // ...other Workbox options...
-    }
-  },
   lintOnSave: false,
   runtimeCompiler: true,
   configureWebpack: {
